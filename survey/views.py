42008a5ae6b6855b2a25e269e5fa3c592091f696
yusseef <yussef.raouf11@gmail.com>
2021-08-15 03:53:13 +0200

    fic post request

--- survey/views.py
+++ survey/views.py
@@ -21,30 +21,26 @@ def Signin(request):
 def Survey(request,pk):
     lst = []
     qs = SurveyUser.objects.filter(pk=pk)
-    User = SurveyUser.objects.get(id = pk)
-    print(User)
+    User = SurveyUser.objects.get(pk = pk)
     questions = Questions.objects.all()
-    answer1 = request.POST.get("2")
-    answer2 = request.POST.get("3")
-    answer3 = request.POST.get("4")
-    answer4 = request.POST.get("5")
-    lst.append(answer1)
-    lst.append(answer2)
-    lst.append(answer3)
-    lst.append(answer4)
-    List = [x for x in lst if not x=='no']
-    counter = Counter(List)
-    result = counter.most_common(1)[0][0]
-    Final_result = SurveyResults(survey_user= User, result=result)
-    Final_result.save()
+    if request.method == 'POST':
+        answer1 = request.POST.get("2")
+        answer2 = request.POST.get("3")
+        answer3 = request.POST.get("4")
+        answer4 = request.POST.get("5")
+        print('hi')
+        lst.append(answer1)
+        lst.append(answer2)
+        lst.append(answer3)
+        lst.append(answer4)
+        List = [x for x in lst if not x=='no']
+        counter = Counter(List)
+        result = counter.most_common(1)[0][0]
+        Final_result = SurveyResults(survey_user= User)
+        Final_result.result = result
+        Final_result.save()
     
     context = {'qs':qs, 'questions':questions}
     return render(request, 'survey.html', context)
 
 
-
-def test(request):
-    
-    result = request.POST.get('result')
-    print(result)
-    return render(request, 'test.html', )
