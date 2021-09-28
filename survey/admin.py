e1414619627e83503fe51a86650dc0881316d1aa
yusseef <yussef.raouf11@gmail.com>
2021-09-28 12:47:33 +0200

    export as csv

--- survey/admin.py
+++ survey/admin.py
@@ -3,13 +3,15 @@ from .models import SurveyUser, Questions, SurveyResults
 from import_export.admin import ExportActionMixin
 # Register your models here.
 class UserAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ['id', 'name', 'email']
+    list_display = ['id', 'name', 'email', 'phone']
 
 class QuestionsAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ['id', 'question', 'category']
 
 class SurveyResultsAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ['survey_user', 'result']
+    list_filter = ['survey_user', 'result']
+    search_fields = ['survey_user__email',]
     
     
 
